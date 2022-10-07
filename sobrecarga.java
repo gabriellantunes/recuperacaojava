086fa91804eae5471ddfa521e5bacd60904ed742
Gabriel Antunes <106894238+gabriellantunes@users.noreply.github.com>
2022-10-07 20:15:10 -0300

    Create sobrecarga.java

---
+++ sobrecarga.java
@@ -0,0 +1,19 @@
+class nossoCarro {
+    String cordocarro;
+    String modelodocarro;
+    String marcadocarro;
+
+    public nossoCarro() {
+        this.cordocarro = "vermelho";
+        this.modelodocarro = "2012";
+        this.marcadocarro = "Mercedes";
+    }
+
+    public void vrumvrum(int acelera) {
+        //
+    }
+
+    public void vrumvrum2(double acelera) {
+        //
+    }
+}
