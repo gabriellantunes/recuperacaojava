e63c66930edcc2d68a2c6d3336ee3d31455ff38b
Gabriel Antunes <106894238+gabriellantunes@users.noreply.github.com>
2022-10-07 19:47:40 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+public class veiculo {
+    String modelo;
+    String Velocidade;
+    String combustivel;
+    String Passageiros;
+}
+
+public class carro extends veiculo {
+    String Modelo;
+    String Portas;
+    String Ano;
+}
+
+public class moto extends veiculo {
+    String Tipo;
+    String ano;
+    Float peso;
+}
